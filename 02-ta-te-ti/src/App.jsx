50c6dc61d9404cad4161843d291b62d09f36c325
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-09-05 22:02:27 -0300

    Ta te ti terminado

--- 02-ta-te-ti/src/App.jsx
+++ 02-ta-te-ti/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 const TURNS = {
-  X: "X",
-  O: "O",
+  X: "❌",
+  O: "⚪",
 };
 
 // Componente de cada cuadro
@@ -21,13 +21,48 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+export const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null); // Null no hay ganador, false es EMPATE
+
+  const checkWinner = (boardToCheck) => {
+    // Revisamos las combinaciones, para ver si gano X u O
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[b] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    // Si no hay ganador
+    return null;
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
 
   const updateBoard = (index) => {
     // No actualizamos contenido, si ya tiene algo
-    if (board[index]) return;
+    // Y también verificamos que no haya un ganador
+    if (board[index] || winner) return;
 
     // Actualizamos tablero
     const newBoard = [...board];
@@ -36,6 +71,11 @@ function App() {
 
     // Cambiamos el turno
     turn === TURNS.X ? setTurn(TURNS.O) : setTurn(TURNS.X);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
 
   return (
@@ -55,6 +95,21 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {winner != null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner == false ? "Empate" : "Ganó " + winner}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
