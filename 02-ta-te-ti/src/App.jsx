d2e710cbaa4bccee1c3724a9596965bbef175af8
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-09-07 17:44:37 -0300

    Update App.jsx

--- 02-ta-te-ti/src/App.jsx
+++ 02-ta-te-ti/src/App.jsx
@@ -42,6 +42,10 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
+    // Guardar partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", JSON.stringify(newTurn));
+
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
       confetti();
