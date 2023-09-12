ab28d9f8227e2229e94da11f7550de734a86a3a5
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-09-11 22:05:43 -0300

    Consumiendo fact desde el backend

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,3 +1,24 @@
+import { useState, useEffect } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+
 export function App() {
-  return <h3>App de gatito</h3>;
+  const [fact, setFact] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => setFact(data.fact));
+
+    return () => {
+      second;
+    };
+  }, []);
+
+  return (
+    <main>
+      <h3>App de gatito</h3>
+      {fact && <p> {fact} </p>}
+    </main>
+  );
 }
