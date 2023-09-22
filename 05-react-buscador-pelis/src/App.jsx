27358241dead0c777b041ad731175d20ca49a3d1
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-09-22 00:32:07 -0300

    Aplicando useRef

--- 05-react-buscador-pelis/src/App.jsx
+++ 05-react-buscador-pelis/src/App.jsx
@@ -1,22 +1,34 @@
+import { useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 
 function App() {
-  const { movies: mappedMovies } = useMovies();
+  const { movies } = useMovies();
+  const inputRef = useRef();
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    const value = inputRef.current.value;
+    console.log(value);
+  };
 
   return (
     <div className="page">
       <h2>Buscador de peliculas</h2>
       <header>
-        <form action="" className="form">
-          <input type="text" placeholder="Avengers, Star Wars, The Matrix.." />
-          <button type="submit"> Buscar </button>
+        <form action="" className="form" onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Avengers, Star Wars, The Matrix.."
+          />
+          <button type="submit">Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
