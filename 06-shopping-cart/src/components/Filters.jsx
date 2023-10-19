97550e94846bf996bfe997920064ec54b633551b
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-10-18 22:06:32 -0300

    Mostrando precio mínimo

--- 06-shopping-cart/src/components/Filters.jsx
+++ 06-shopping-cart/src/components/Filters.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./Filters.css";
 
 export function Filters() {
+  const [minPrice, setMinPrice] = useState(0);
+
+  const handleChange = (event) => {
+    setMinPrice(event.target.value);
+  };
+
   return (
     <section className="filters">
       <div>
-        <label htmlFor="price">Precio</label>
-        <input type="range" id="price" min="0" max="10000" />
+        <label htmlFor="price">Precio mínimo</label>
+        <input
+          type="range"
+          id="price"
+          min="0"
+          max="10000"
+          onChange={handleChange}
+        />
+        <span>$ {minPrice}</span>
       </div>
 
       <div>
