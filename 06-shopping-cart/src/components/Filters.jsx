66237a115d124adfa925d31f4160524e6dfb57ce
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-10-25 23:08:42 -0300

    Usando ids desde react

--- 06-shopping-cart/src/components/Filters.jsx
+++ 06-shopping-cart/src/components/Filters.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 import "./Filters.css";
 
 export function Filters({ onChange }) {
   const [minPrice, setMinPrice] = useState(0);
+
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
 
   const handleChange = (event) => {
     setMinPrice(event.target.value);
@@ -22,10 +25,10 @@ export function Filters({ onChange }) {
   return (
     <section className="filters">
       <div>
-        <label htmlFor="price">Precio mínimo</label>
+        <label htmlFor={minPriceFilterId}>Precio mínimo</label>
         <input
           type="range"
-          id="price"
+          id={minPriceFilterId}
           min="0"
           max="1500"
           onChange={handleChange}
@@ -34,8 +37,8 @@ export function Filters({ onChange }) {
       </div>
 
       <div>
-        <label htmlFor="category">Categoria</label>
-        <select id="category" onChange={handleChangeCategory}>
+        <label htmlFor={categoryFilterId}>Categoria</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
           <option value="all">Todas</option>
           <option value="laptops">Portátiles</option>
           <option value="smartphones">Celulares</option>
