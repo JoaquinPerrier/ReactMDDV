7999cf8690671f666ee3c710a3eb427f6a0ae997
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-10-31 20:23:41 -0300

    Pasando el usefilters a un archivo aparte

--- 06-shopping-cart/src/App.jsx
+++ 06-shopping-cart/src/App.jsx
@@ -1,29 +1,9 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Products } from "./components/Products";
 import { products as initialProducts } from "./mocks/products.json";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { FiltersContext } from "./context/filters";
-
-function useFilters() {
-  /*const [filters, setFilters] = useState({
-    category: "all",
-    minPrice: 0,
-  });*/
-
-  const { filters, setFilters } = useContext(FiltersContext);
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category == "all" || product.category == filters.category)
-      );
-    });
-  };
-
-  return { filterProducts, setFilters, filters };
-}
+import { useFilters } from "./hooks/useFilters";
 
 function App() {
   const [products] = useState(initialProducts);
