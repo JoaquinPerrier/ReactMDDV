00a56bc41d2257c251b1f487a9e96c4bd602edec
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-11-02 18:38:59 -0300

    Agregando carrito

---
+++ 06-shopping-cart/src/components/Cart.jsx
@@ -0,0 +1,37 @@
+import { useId } from "react";
+import { ClearCartIcon, RemoveFromCartIcon, CartIcon } from "./Icons";
+
+export function Cart() {
+  const cartCheckbocId = useId();
+
+  return (
+    <>
+      <label htmlFor={cartCheckbocId} className="cart-button">
+        <CartIcon />
+      </label>
+      <input type="checkbox" id={cartCheckbocId} hidden />
+
+      <aside className="cart">
+        <ul>
+          <li>
+            <img
+              src="https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+              alt="iphone"
+            />
+            <div>
+              <strong>Iphone</strong> - $899
+            </div>
+            <footer>
+              <small>Qty: 1</small>
+              <button>+</button>
+            </footer>
+          </li>
+        </ul>
+
+        <button>
+          <ClearCartIcon />
+        </button>
+      </aside>
+    </>
+  );
+}
