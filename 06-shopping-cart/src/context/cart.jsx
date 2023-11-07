878692bed22c9b920155d333dbabf243240dbef6
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-11-06 23:59:32 -0300

    Update cart.jsx

--- 06-shopping-cart/src/context/cart.jsx
+++ 06-shopping-cart/src/context/cart.jsx
@@ -9,21 +9,20 @@ export function CartProvider({ children }) {
 
   const addToCart = (product) => {
     // Vemos si el producto está en el carrito, para ver si aumentamos cantidad o lo tenemos que agregar
-    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
-    console.log(productInCartIndex);
+    const productInCartIndex = cart.findIndex((item) => item.id == product.id);
 
     if (productInCartIndex >= 0) {
       // SI EL PRODUCTO YA ESTÁ EN EL CARRITO
       const newCart = structuredClone(cart);
       newCart[productInCartIndex].quantity += 1;
-      setCart(newCart);
+      return setCart(newCart);
     }
 
     // SI EL PRODUCTO NO ESTÁ EN EL CARRITO
     setCart((prevState) => [
       ...prevState,
       {
-        product,
+        ...product,
         quantity: 1,
       },
     ]);
