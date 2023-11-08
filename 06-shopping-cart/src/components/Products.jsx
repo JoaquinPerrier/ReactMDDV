fd4b7d7a13ff846a053892adeb589d510a2f8602
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-11-07 23:06:24 -0300

    Cambiando el ícono dependiendo si el elemento está o no en el carrito

--- 06-shopping-cart/src/components/Products.jsx
+++ 06-shopping-cart/src/components/Products.jsx
@@ -1,28 +1,35 @@
 import "./Products.css";
-import { AddToCartIcon } from "./Icons";
+import { AddToCartIcon, RemoveFromCartIcon } from "./Icons";
 import { useCart } from "../hooks/useCart";
 
 export function Products({ products }) {
   const { addToCart, cart } = useCart();
 
-  console.log(cart);
+  // Para ver los cambios
+  const checkProductInCart = (product) => {
+    return cart.some((item) => item.id === product.id);
+  };
 
   return (
     <main className="products">
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <img src={product.thumbnail} alt={product.title} />
-            <div>
-              <strong>{product.title}</strong> - ${product.price}
-            </div>
-            <div>
-              <button onClick={() => addToCart(product)}>
-                <AddToCartIcon />
-              </button>
-            </div>
-          </li>
-        ))}
+        {products.map((product) => {
+          const isProductInCart = checkProductInCart(product);
+
+          return (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
+              <div>
+                <button onClick={() => addToCart(product)}>
+                  {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
+                </button>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
